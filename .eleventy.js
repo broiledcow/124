446ef535caa313d20f4db4777d5a81b2b0208438
WrenKennison <wrenkenson@gmail.com>
2025-07-20 05:07:05 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,10 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/style.css')
 	eleventyConfig.addPassthroughCopy('./src/style.css')
 	eleventyConfig.addPassthroughCopy('./src/assets/')
+	eleventyConfig.addPassthroughCopy('./src/assets/pfps/')
+	eleventyConfig.addPassthroughCopy('./src/assets/releases/')
+	eleventyConfig.addPassthroughCopy('./src/assets/css/')
+
 	eleventyConfig.addPassthroughCopy('./src/admin')
 	eleventyConfig.addFilter("postDate", (dateObj) => { return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED) } );
   	eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
